a802c99512f9f388a62c16c63674cddde1ca9c1b
Tiago Sartor <contact.me@tiagosartor.dev>
2026-09-05 20:23:44 -0300

    Update minimum PHP version to 8.2, WordPress version tested up to 7.1, and WooCommerce version tested up to 11.1

--- wc-attribute-fees.php
+++ wc-attribute-fees.php
@@ -10,10 +10,10 @@
  * License URI:        https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:        storestack-attribute-fees-for-woocommerce
  * Requires at least:  6.2
- * Tested up to:       7.0
+ * Tested up to:       7.1
  * Requires Plugins:   woocommerce
- * WC tested up to:    10.3
- * Requires PHP:       8.1
+ * WC tested up to:    11.1
+ * Requires PHP:       8.2
  *
  * @package StoreStackAttributeFeesForWooCommerce
  */
